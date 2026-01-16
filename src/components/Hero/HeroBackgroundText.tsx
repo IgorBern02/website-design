916183efdd34bdd461e63e91b5319f294edf26af
Igor Bernardes Seixas <igor.bernardes2@gmail.com>
2026-01-16 18:45:12 -0300

    chore: add font roboto

--- src/components/Hero/HeroBackgroundText.tsx
+++ src/components/Hero/HeroBackgroundText.tsx
@@ -10,8 +10,31 @@ export const HeroBackgroundText = ({ scrollYProgress }: any) => {
       className="absolute inset-0 flex justify-center mt-18 pointer-events-none"
     >
       <div className="flex gap-60 text-black">
-        <span className="text-[9rem] font-bold font-poppins">Aura</span>
-        <span className="text-[9rem] font-bold font-poppins">store</span>
+        <span
+          className="text-[9rem] font-bold font-poppins"
+          style={{
+            textShadow: `
+      0 4px 10px rgba(0,0,0,0.25),
+      0 20px 40px rgba(0,0,0,0.35),
+      0 0 40px rgba(255,255,255,0.15)
+    `,
+          }}
+        >
+          Aura
+        </span>
+
+        <span
+          className="text-[9rem] font-bold font-poppins"
+          style={{
+            textShadow: `
+      0 4px 10px rgba(0,0,0,0.25),
+      0 20px 40px rgba(0,0,0,0.35),
+      0 0 40px rgba(255,255,255,0.15)
+    `,
+          }}
+        >
+          store
+        </span>
       </div>
     </motion.div>
   );
