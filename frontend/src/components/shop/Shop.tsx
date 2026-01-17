cc8843be25f75230114115d40075c89615ba5311
Igor Bernardes Seixas <igor.bernardes2@gmail.com>
2026-01-17 17:29:18 -0300

    refactor: add frontend folder

--- frontend/src/components/shop/Shop.tsx
+++ frontend/src/components/shop/Shop.tsx
@@ -5,8 +5,12 @@ export const Shop = () => {
     <section className="min-h-screen pt-28 px-20  flex flex-col gap-10">
       <h2 className="text-8xl font-black font-roboto ">SHOP</h2>
       <section className="w-full h-80  ">
-        <div className="border w-1/3 h-full flex justify-center items-center rounded-2xl shadow-lg gap-6 p-6">
-          <img src={card1} alt="card1" className="h-100 scale-100 z-90 mb-20" />
+        <div className="border w-100 h-full flex justify-center items-center rounded-2xl shadow-lg ">
+          <img
+            src={card1}
+            alt="card1"
+            className="w-full h-100 scale-105 z-90 mb-25 object-contain"
+          />
         </div>
         <div>
           <h3 className="text-4xl font-bold">Product Name</h3>
