1e80f1dfd52c708563cfb18d792fb994c0769517
Igor Bernardes Seixas <igor.bernardes2@gmail.com>
2026-01-26 18:51:33 -0300

    refactor: font awesome icons refactored

--- frontend/src/components/shop/ProductPage/ProductData/ButtonBuy.tsx
+++ frontend/src/components/shop/ProductPage/ProductData/ButtonBuy.tsx
@@ -1,17 +1,21 @@
-import { faHeart } from "@fortawesome/free-solid-svg-icons";
+import { useState } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 type ButtonBuyProps = { text?: string };
 
 export const ButtonBuy = ({ text }: ButtonBuyProps) => {
+  const [liked, setLiked] = useState(false);
+
   return (
     <div className="flex justify-between items-center">
-      <button className="w-full border rounded-2xl text-white px-10 py-4 font-comfortaa text-sm hover:bg-gray-100 transition  cursor-pointer">
+      <button className="w-full border rounded-2xl text-white px-10 py-4 font-comfortaa text-sm hover:bg-gray-100 transition cursor-pointer">
         {text || "Comprar agora"}
       </button>
+
       <FontAwesomeIcon
-        icon={faHeart}
-        className="text-xl text-gray-700 p-4 cursor-pointer  rounded-full hover:bg-gray-100 ml-4 shadow-sm shadow-gray-300"
+        icon={liked ? ["fas", "heart"] : ["far", "heart"]}
+        onClick={() => setLiked(!liked)}
+        className="text-xl text-gray-700 p-4 cursor-pointer rounded-full hover:bg-gray-100 ml-4 shadow-sm shadow-gray-300"
       />
     </div>
   );
