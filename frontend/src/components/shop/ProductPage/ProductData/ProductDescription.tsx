e3a4b37ba263ea74a7822e6497ee14475f352ee1
Igor Bernardes Seixas <igor.bernardes2@gmail.com>
2026-01-26 18:50:15 -0300

    feat: props in the Description

--- frontend/src/components/shop/ProductPage/ProductData/ProductDescription.tsx
+++ frontend/src/components/shop/ProductPage/ProductData/ProductDescription.tsx
@@ -1,14 +1,17 @@
-export const ProductDescription = () => {
+type ProductDescriptionProps = {
+  description: string;
+};
+
+export const ProductDescription = ({
+  description,
+}: ProductDescriptionProps) => {
   return (
     <div className="w-full">
       <h2 className="font-poppins text-sm font-semibold mb-4">
         Description & Fit
       </h2>
       <p className="font-poppins text-sm font-light leading-6 text-gray-600">
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias
-        debitis eaque ipsum minus pariatur officia reprehenderit qui, deserunt
-        sint tempora voluptate corporis repellat temporibus quisquam fugit.
-        Eligendi ipsa adipisci magni.
+        {description}
       </p>
     </div>
   );
